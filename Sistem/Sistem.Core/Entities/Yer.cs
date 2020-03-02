10e4e120ae9af2cc8d20ab827bf436b266a15b49
Şükrü Berkay(bipro) <biproberkay@outlook.com>
2020-03-02 21:09:32 +0300

    şimdilik olmadı

--- Sistem/Sistem.Core/Entities/Yer.cs
+++ Sistem/Sistem.Core/Entities/Yer.cs
@@ -9,13 +9,6 @@ namespace Sistem.Core.Entities
         public int Id { get; set; }
         public string Title { get; set; }
 
-        public Yer()
-        {
-            YerChilds = new HashSet<Yer>();
-        }
-        public int Level { get; set; }
-        public int? ParentId { get; set; }
-        public Yer Parent { get; set; }
-        public virtual ICollection<Yer> YerChilds { get; set; }
+        //public HierarchyId Level { get; set; }
     }
 }
