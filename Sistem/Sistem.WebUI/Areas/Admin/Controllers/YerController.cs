6ed89131eb3e15f7fb80976e9b25d90b22a498ba
Şükrü Berkay(bipro) <biproberkay@outlook.com>
2020-03-11 20:23:59 +0300

    yer yönetimş eklendi

--- Sistem/Sistem.WebUI/Areas/Admin/Controllers/YerController.cs
+++ Sistem/Sistem.WebUI/Areas/Admin/Controllers/YerController.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Sistem.Core.Abstract.ServiceInterfaces;
 using Sistem.Core.Entities;
+using Sistem.WebUI.Controllers;
 
-namespace Sistem.WebUI.Controllers
+namespace Sistem.WebUI.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class YerController : DefaultController<Yer>
     {
         public YerController(IServiceRepository<Yer> tService) : base(tService)
